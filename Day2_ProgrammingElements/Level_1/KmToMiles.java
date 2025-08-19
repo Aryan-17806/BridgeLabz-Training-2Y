45495b019253863be3006c2dcf0fe3cd5cb3ca72
Aryan Singh <aryansingh17806@gmail.com>
2025-08-19 15:28:17 +0530

    Day2_ProgrammingElements

--- Day2_ProgrammingElements/Level_1/KmToMiles.java
+++
@@ -1,9 +0,0 @@
-// Program 3: Convert 10.8 Kilometers to Miles
-public class KmToMiles {
-    public static void main(String[] args) {
-        double km = 10.8;
-        double milesPerKm = 1.6;
-        double miles = km / milesPerKm;
-        System.out.println(km + " kilometers in miles is " + miles);
-    }
-}
