366c42f33e30dc9ca51ba1b8a56d1b6dd5828b93
Aryan Singh <aryansingh17806@gmail.com>
2025-10-27 14:41:07 +0530

    day

---
+++ Packages_in_Java/MainApp_2.java
@@ -0,0 +1,9 @@
+import com.university.department.cse.Course;
+import com.university.department.cse.*;
+
+public class MainApp_2 {
+    public static void main(String[] args) {
+        Course c = new Course();
+        c.showCourse();
+    }
+}
